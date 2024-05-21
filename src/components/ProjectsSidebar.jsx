2a2a81b4a884ecc29b9f4443672bdbd7b84ddef3
Alfio Puglisi <puglisi_algio@ciao.it>
2024-05-21 18:25:14 +0200

    Inserimento titolo progetti nella sidebar

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,16 +1,23 @@
 import Button from "./Button";
 
-export default function ProjectsSidebar({ onStartAddProject }) {
+export default function ProjectsSidebar({ onStartAddProject, projects }) {
   return (
     <aside className="bg-stone-900 text-stone-50 w-1/3 px-8 py-16 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         I tuoi Progetti
       </h2>
       <div>
-        <Button onClick={onStartAddProject}>
-          + Nuovo Progetto
-        </Button>
+        <Button onClick={onStartAddProject}>+ Nuovo Progetto</Button>
       </div>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+              {project.titolo}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
