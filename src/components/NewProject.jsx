48157ecb7b77ba44916a209c9b2ae4a9234e4ad2
Alfio Puglisi <puglisi_algio@ciao.it>
2024-05-22 12:11:34 +0200

    Verifica della validità dei campi e funzionalità del bottone "Elimina"

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,40 +1,65 @@
 import { useRef } from "react";
+
 import Input from "./Input.jsx";
+import Error from "./Error.jsx";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
 
-const titolo = useRef();
-const descrizone = useRef();
-const scadenza = useRef();
+  const modal = useRef();
+  const titolo = useRef();
+  const descrizone = useRef();
+  const scadenza = useRef();
 
-function handleSave(){
-  const titoloInserito = titolo.current.value;
-  const descrizioneInserita = descrizone.current.value;
-  const scadenzaInserita = scadenza.current.value;
+  function handleSave() {
+    const titoloInserito = titolo.current.value;
+    const descrizioneInserita = descrizone.current.value;
+    const scadenzaInserita = scadenza.current.value;
 
-  //valdiazione...
-  onAdd({
-    titolo: titoloInserito,
-    descrizone: descrizioneInserita,
-    scadenza: scadenzaInserita
-  });
-}
+    if (
+      titoloInserito.trim() === "" ||
+      descrizioneInserita.trim() === "" ||
+      scadenzaInserita.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
+    //valdiazione...
+    onAdd({
+      titolo: titoloInserito,
+      descrizione: descrizioneInserita,
+      scadenza: scadenzaInserita,
+    });
+  }
 
   return (
-    <div className="w-[35rem] mt-16 ">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">Elimina</button>
-        </li>
-        <li>
-          <button className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md" onClick={handleSave}>Salva</button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" label="Titolo" ref={titolo}/>
-        <Input label="Descrizione" textarea ref={descrizone}/>
-        <Input type="date" label="Scadenza" ref={scadenza}/>
+    <>
+      <Error ref={modal} buttonContent="Chiudi">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Dati non validi</h2>
+        <p className="text-stone-600 mb-4">Riempi tutti i campi per proseguire</p>
+      </Error>
+      <div className="w-[35rem] mt-16 ">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
+              Elimina
+            </button>
+          </li>
+          <li>
+            <button
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+              onClick={handleSave}
+            >
+              Salva
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" label="Titolo" ref={titolo} />
+          <Input label="Descrizione" textarea ref={descrizone} />
+          <Input type="date" label="Scadenza" ref={scadenza} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
